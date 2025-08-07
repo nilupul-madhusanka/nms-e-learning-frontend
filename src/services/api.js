b26f5573dc876bbce4a17cdbbabe9eaeb1071413
nilupul-madhusanka <nilupulmadhusanka2001m31@gmail.com>
2025-08-07 18:41:10 +0530

    2

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://real-goats-remain.loca.lt/api';
+const API_BASE_URL = 'https://spicy-beers-sort.loca.lt/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
